fdbb5bd337895f469787f0cfe842b9ddf4effcb0
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-04 18:13:54 +0800

    Create Beanrecommend_menu.java

---
+++ freshnetwork/src/hk/freshnetwork/model/Beanrecommend_menu.java
@@ -0,0 +1,25 @@
+package hk.freshnetwork.model;
+
+public class Beanrecommend_menu {
+    private int Men_Menu_number;
+    private int Com_Trade_number;
+    private String miaoshu;
+	public int getMen_Menu_number() {
+		return Men_Menu_number;
+	}
+	public void setMen_Menu_number(int men_Menu_number) {
+		Men_Menu_number = men_Menu_number;
+	}
+	public int getCom_Trade_number() {
+		return Com_Trade_number;
+	}
+	public void setCom_Trade_number(int com_Trade_number) {
+		Com_Trade_number = com_Trade_number;
+	}
+	public String getMiaoshu() {
+		return miaoshu;
+	}
+	public void setMiaoshu(String miaoshu) {
+		this.miaoshu = miaoshu;
+	}
+}
