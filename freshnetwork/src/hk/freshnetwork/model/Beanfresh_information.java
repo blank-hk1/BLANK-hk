1d0a37f75dcad57f356e3317f68587860bc97b1a
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-04 18:13:39 +0800

    Create Beanfresh_information.java

---
+++ freshnetwork/src/hk/freshnetwork/model/Beanfresh_information.java
@@ -0,0 +1,25 @@
+package hk.freshnetwork.model;
+
+public class Beanfresh_information {
+    private int Category_number;
+    private String Category_name;
+    private String Category_description;
+	public int getCategory_number() {
+		return Category_number;
+	}
+	public void setCategory_number(int category_number) {
+		Category_number = category_number;
+	}
+	public String getCategory_name() {
+		return Category_name;
+	}
+	public void setCategory_name(String category_name) {
+		Category_name = category_name;
+	}
+	public String getCategory_description() {
+		return Category_description;
+	}
+	public void setCategory_description(String category_description) {
+		Category_description = category_description;
+	}
+}
