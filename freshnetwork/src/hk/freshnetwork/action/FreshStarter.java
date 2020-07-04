058dc8ceb0ab45fe2144d437da25c2eced45a807
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-04 18:13:10 +0800

    Create FreshStarter.java

---
+++ freshnetwork/src/hk/freshnetwork/action/FreshStarter.java
@@ -0,0 +1,13 @@
+package hk.freshnetwork.action;
+
+import hk.freshnetwork.ui.FrmMain;
+
+public class FreshStarter {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new FrmMain();
+	}
+}
