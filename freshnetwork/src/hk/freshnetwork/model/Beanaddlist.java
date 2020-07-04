bb12566e669fd0d74bba9ffd92f85faa1ac90bf4
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-04 18:13:30 +0800

    Create Beanaddlist.java

---
+++ freshnetwork/src/hk/freshnetwork/model/Beanaddlist.java
@@ -0,0 +1,67 @@
+package hk.freshnetwork.model;
+
+public class Beanaddlist {
+     private int add_number;
+     private int User_num;
+     private int ord_number;
+     private String sheng;
+     private String shi;
+     private String qu;
+     private String address;
+     private String contacts;
+     private String con_phone;
+	public int getAdd_number() {
+		return add_number;
+	}
+	public void setAdd_number(int add_number) {
+		this.add_number = add_number;
+	}
+	public int getUser_num() {
+		return User_num;
+	}
+	public void setUser_num(int user_num) {
+		User_num = user_num;
+	}
+	public int getOrd_number() {
+		return ord_number;
+	}
+	public void setOrd_number(int ord_number) {
+		this.ord_number = ord_number;
+	}
+	public String getSheng() {
+		return sheng;
+	}
+	public void setSheng(String sheng) {
+		this.sheng = sheng;
+	}
+	public String getShi() {
+		return shi;
+	}
+	public void setShi(String shi) {
+		this.shi = shi;
+	}
+	public String getQu() {
+		return qu;
+	}
+	public void setQu(String qu) {
+		this.qu = qu;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getContacts() {
+		return contacts;
+	}
+	public void setContacts(String contacts) {
+		this.contacts = contacts;
+	}
+	public String getCon_phone() {
+		return con_phone;
+	}
+	public void setCon_phone(String con_phone) {
+		this.con_phone = con_phone;
+	}
+}
