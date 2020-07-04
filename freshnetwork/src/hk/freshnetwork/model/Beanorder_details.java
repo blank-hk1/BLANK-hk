10c28a8cae4bee3109f39742f9759dcf76a8737f
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-04 18:13:48 +0800

    Create Beanorder_details.java

---
+++ freshnetwork/src/hk/freshnetwork/model/Beanorder_details.java
@@ -0,0 +1,46 @@
+package hk.freshnetwork.model;
+
+public class Beanorder_details {
+      private int ord_ord_number;
+      private int Com_Trade_number;
+      private int Ful_Full_number;
+      private int pur_number;
+      private float Discount;
+      private float set_money;
+	public int getOrd_ord_number() {
+		return ord_ord_number;
+	}
+	public void setOrd_ord_number(int ord_ord_number) {
+		this.ord_ord_number = ord_ord_number;
+	}
+	public int getCom_Trade_number() {
+		return Com_Trade_number;
+	}
+	public void setCom_Trade_number(int com_Trade_number) {
+		Com_Trade_number = com_Trade_number;
+	}
+	public int getFul_Full_number() {
+		return Ful_Full_number;
+	}
+	public void setFul_Full_number(int ful_Full_number) {
+		Ful_Full_number = ful_Full_number;
+	}
+	public int getPur_number() {
+		return pur_number;
+	}
+	public void setPur_number(int pur_number) {
+		this.pur_number = pur_number;
+	}
+	public float getDiscount() {
+		return Discount;
+	}
+	public void setDiscount(float discount) {
+		Discount = discount;
+	}
+	public float getSet_money() {
+		return set_money;
+	}
+	public void setSet_money(float set_money) {
+		this.set_money = set_money;
+	}
+}
