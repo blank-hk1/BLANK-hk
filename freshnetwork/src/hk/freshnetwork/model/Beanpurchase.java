ab243ccfcd80391e20be4252fd0edc883700f666
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-04 18:13:52 +0800

    Create Beanpurchase.java

---
+++ freshnetwork/src/hk/freshnetwork/model/Beanpurchase.java
@@ -0,0 +1,32 @@
+package hk.freshnetwork.model;
+
+public class Beanpurchase {
+  private int chase_number;
+  private int Emp_number;
+  private int purchase_amount;
+  private String chase_stat;
+public int getChase_number() {
+	return chase_number;
+}
+public void setChase_number(int chase_number) {
+	this.chase_number = chase_number;
+}
+public int getEmp_number() {
+	return Emp_number;
+}
+public void setEmp_number(int emp_number) {
+	Emp_number = emp_number;
+}
+public int getPurchase_amount() {
+	return purchase_amount;
+}
+public void setPurchase_amount(int purchase_amount) {
+	this.purchase_amount = purchase_amount;
+}
+public String getChase_stat() {
+	return chase_stat;
+}
+public void setChase_stat(String chase_stat) {
+	this.chase_stat = chase_stat;
+}
+}
