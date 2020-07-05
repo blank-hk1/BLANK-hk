0e6f84a14669e5dee255f98c6c95569aade18147
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-05 16:30:55 +0800

    Update FrmMain.java

--- freshnetwork/src/hk/freshnetwork/ui/FrmMain.java
+++ freshnetwork/src/hk/freshnetwork/ui/FrmMain.java
@@ -22,6 +22,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import hk.freshnetwork.action.FreshNetUtil;
 import hk.freshnetwork.util.BaseException;
 
 
@@ -31,10 +32,15 @@ public class FrmMain extends JFrame implements ActionListener {
 	public static int Identity;
 	private static final long serialVersionUID = 1L;
 	private JMenuBar menubar=new JMenuBar(); ;
-    private JMenu menu_plan=new JMenu("计划管理");
-    private JMenu menu_step=new JMenu("步骤管理");
-    private JMenu menu_static=new JMenu("查询统计");
-    private JMenu menu_more=new JMenu("更多");
+    private JMenu fresh_back=new JMenu("生鲜后台管理");
+    private JMenu Discount_back=new JMenu("优惠后台管理");
+    private JMenu User_back=new JMenu("用户管理");
+    private JMenu admin_person=new JMenu("更多");
+    
+    private JMenu person_plan=new JMenu("生鲜后台管理");
+    private JMenu person_step=new JMenu("优惠后台管理");
+    private JMenu person_static=new JMenu("用户管理");
+    private JMenu person_more=new JMenu("个人中心");
     
     private JMenuItem  menuItem_AddPlan=new JMenuItem("新建计划");
     private JMenuItem  menuItem_DeletePlan=new JMenuItem("删除计划");
@@ -45,7 +51,14 @@ public class FrmMain extends JFrame implements ActionListener {
     private JMenuItem  menuItem_moveUpStep=new JMenuItem("步骤上移");
     private JMenuItem  menuItem_moveDownStep=new JMenuItem("步骤下移");
     
-    private JMenuItem  menuItem_modifyPwd=new JMenuItem("密码修改");
+    private JMenuItem  person_modifyPwd=new JMenuItem("修改密码");
+    private JMenuItem  person_modifyphone=new JMenuItem("修改手机号码");
+    private JMenuItem  person_modifymail=new JMenuItem("修改邮箱");
+    private JMenuItem  person_modifycity=new JMenuItem("修改所在城市");
+    private JMenuItem  person_openvip=new JMenuItem("会员中心");
+    
+    private JMenuItem  admin_modifyPwd=new JMenuItem("密码修改");
+    private JMenuItem  admin_add=new JMenuItem("添加管理员");
     
     private JMenuItem  menuItem_static1=new JMenuItem("统计1");
     
@@ -110,21 +123,25 @@ public class FrmMain extends JFrame implements ActionListener {
 			this.setTitle("生鲜海超用户系统");
 			
 		    //菜单
-		    this.menu_plan.add(this.menuItem_AddPlan); this.menuItem_AddPlan.addActionListener(this);
-		    this.menu_plan.add(this.menuItem_DeletePlan); this.menuItem_DeletePlan.addActionListener(this);
-		    this.menu_step.add(this.menuItem_AddStep); this.menuItem_AddStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_DeleteStep); this.menuItem_DeleteStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_startStep); this.menuItem_startStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_finishStep); this.menuItem_finishStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_moveUpStep); this.menuItem_moveUpStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_moveDownStep); this.menuItem_moveDownStep.addActionListener(this);
-		    this.menu_static.add(this.menuItem_static1); this.menuItem_static1.addActionListener(this);
-		    this.menu_more.add(this.menuItem_modifyPwd); this.menuItem_modifyPwd.addActionListener(this);
+		    this.person_plan.add(this.menuItem_AddPlan); this.menuItem_AddPlan.addActionListener(this);
+		    this.person_plan.add(this.menuItem_DeletePlan); this.menuItem_DeletePlan.addActionListener(this);
+		    this.person_step.add(this.menuItem_AddStep); this.menuItem_AddStep.addActionListener(this);
+		    this.person_step.add(this.menuItem_DeleteStep); this.menuItem_DeleteStep.addActionListener(this);
+		    this.person_step.add(this.menuItem_startStep); this.menuItem_startStep.addActionListener(this);
+		    this.person_step.add(this.menuItem_finishStep); this.menuItem_finishStep.addActionListener(this);
+		    this.person_step.add(this.menuItem_moveUpStep); this.menuItem_moveUpStep.addActionListener(this);
+		    this.person_step.add(this.menuItem_moveDownStep); this.menuItem_moveDownStep.addActionListener(this);
+		    this.person_static.add(this.menuItem_static1); this.menuItem_static1.addActionListener(this);
+		    this.person_more.add(this.person_modifyPwd); this.person_modifyPwd.addActionListener(this);
+		    this.person_more.add(this.person_modifyphone); this.person_modifyphone.addActionListener(this);
+		    this.person_more.add(this.person_modifymail); this.person_modifymail.addActionListener(this);
+		    this.person_more.add(this.person_modifycity); this.person_modifycity.addActionListener(this);
+		    this.person_more.add(this.person_openvip); this.person_openvip.addActionListener(this);
 		    
-		    menubar.add(menu_plan);
-		    menubar.add(menu_step);
-		    menubar.add(menu_static);
-		    menubar.add(menu_more);
+		    menubar.add(person_plan);
+		    menubar.add(person_step);
+		    menubar.add(person_static);
+		    menubar.add(person_more);
 		    this.setJMenuBar(menubar);
 		    
 		    /*this.getContentPane().add(new JScrollPane(this.dataTablePlan), BorderLayout.WEST);
@@ -160,21 +177,22 @@ public class FrmMain extends JFrame implements ActionListener {
 			this.setTitle("生鲜海超管理系统");
 			
 		    //菜单
-		    this.menu_plan.add(this.menuItem_AddPlan); this.menuItem_AddPlan.addActionListener(this);
-		    this.menu_plan.add(this.menuItem_DeletePlan); this.menuItem_DeletePlan.addActionListener(this);
-		    this.menu_step.add(this.menuItem_AddStep); this.menuItem_AddStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_DeleteStep); this.menuItem_DeleteStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_startStep); this.menuItem_startStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_finishStep); this.menuItem_finishStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_moveUpStep); this.menuItem_moveUpStep.addActionListener(this);
-		    this.menu_step.add(this.menuItem_moveDownStep); this.menuItem_moveDownStep.addActionListener(this);
-		    this.menu_static.add(this.menuItem_static1); this.menuItem_static1.addActionListener(this);
-		    this.menu_more.add(this.menuItem_modifyPwd); this.menuItem_modifyPwd.addActionListener(this);
+		    this.fresh_back.add(this.menuItem_AddPlan); this.menuItem_AddPlan.addActionListener(this);
+		    this.fresh_back.add(this.menuItem_DeletePlan); this.menuItem_DeletePlan.addActionListener(this);
+		    this.Discount_back.add(this.menuItem_AddStep); this.menuItem_AddStep.addActionListener(this);
+		    this.Discount_back.add(this.menuItem_DeleteStep); this.menuItem_DeleteStep.addActionListener(this);
+		    this.Discount_back.add(this.menuItem_startStep); this.menuItem_startStep.addActionListener(this);
+		    this.Discount_back.add(this.menuItem_finishStep); this.menuItem_finishStep.addActionListener(this);
+		    this.Discount_back.add(this.menuItem_moveUpStep); this.menuItem_moveUpStep.addActionListener(this);
+		    this.Discount_back.add(this.menuItem_moveDownStep); this.menuItem_moveDownStep.addActionListener(this);
+		    this.User_back.add(this.menuItem_static1); this.menuItem_static1.addActionListener(this);
+		    this.admin_person.add(this.admin_modifyPwd); this.admin_modifyPwd.addActionListener(this);
+		    this.admin_person.add(this.admin_add); this.admin_add.addActionListener(this);
 		    
-		    menubar.add(menu_plan);
-		    menubar.add(menu_step);
-		    menubar.add(menu_static);
-		    menubar.add(menu_more);
+		    menubar.add(fresh_back);
+		    menubar.add(Discount_back);
+		    menubar.add(User_back);
+		    menubar.add(admin_person);
 		    this.setJMenuBar(menubar);
 		    
 		    /*this.getContentPane().add(new JScrollPane(this.dataTablePlan), BorderLayout.WEST);
@@ -207,14 +225,8 @@ public class FrmMain extends JFrame implements ActionListener {
 		}
 		
 	}
-
-	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-	/*public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==this.menuItem_AddPlan){
+		/*if(e.getSource()==this.menuItem_AddPlan){
 			FrmAddPlan dlg=new FrmAddPlan(this,"添加计划",true);
 			dlg.setVisible(true);
 		}
@@ -288,7 +300,7 @@ public class FrmMain extends JFrame implements ActionListener {
 			}
 		}
 		else if(e.getSource()==this.menuItem_moveDownStep){
-			int i=FrmMain.this.dataTableStep.getSelectedRow();
+			int i=FrmMain.this.dataTableStep.getSelectedRow();S
 			if(i<0) {
 				JOptionPane.showMessageDialog(null, "请选择步骤", "错误",JOptionPane.ERROR_MESSAGE);
 				return;
@@ -302,11 +314,35 @@ public class FrmMain extends JFrame implements ActionListener {
 		}
 		else if(e.getSource()==this.menuItem_static1){
 			
+		}*/
+		if(e.getSource()==this.admin_modifyPwd){
+			FrmModifyPwd dlg=new FrmModifyPwd(this,"密码修改",true);
+			dlg.setVisible(true);
 		}
-		else if(e.getSource()==this.menuItem_modifyPwd){
-			//FrmModifyPwd dlg=new FrmModifyPwd(this,"密码修改",true);
-			//dlg.setVisible(true);
+		else if(e.getSource()==this.admin_add) {
+			FrmaddAdm adm = new FrmaddAdm(this,"添加管理员",true);
+			adm.setVisible(true);
 		}
-	}*/
+		else if(e.getSource()==this.person_modifyPwd) {
+			FrmUserPwd dlg = new FrmUserPwd(this,"修改密码",true);
+			dlg.setVisible(true);
+		}
+		else if(e.getSource()==this.person_modifyphone) {
+			FrmModifyphone dlg = new FrmModifyphone(this,"修改手机号码",true);
+			dlg.setVisible(true);
+		}
+		else if(e.getSource()==this.person_modifymail) {
+			FrmModifymail dlg = new FrmModifymail(this,"修改邮箱",true);
+			dlg.setVisible(true);
+		}
+		else if(e.getSource()==this.person_modifycity) {
+			FrmModifyCity dlg = new FrmModifyCity(this,"修改所在城市",true);
+			dlg.setVisible(true);
+		}
+		else if(e.getSource()==this.person_openvip) {
+			FrmOpenVip dlg = new FrmOpenVip(this,"会员中心",true);
+			dlg.setVisible(true);
+	}
 	
+  }
 }
