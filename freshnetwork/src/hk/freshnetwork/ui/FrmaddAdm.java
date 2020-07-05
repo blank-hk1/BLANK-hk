b3d86b67adc45913d1f1667852bad0d45f5e3c3b
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-05 16:30:53 +0800

    Create FrmaddAdm.java

---
+++ freshnetwork/src/hk/freshnetwork/ui/FrmaddAdm.java
@@ -0,0 +1,81 @@
+package hk.freshnetwork.ui;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+import hk.freshnetwork.control.ExampleUserManager;
+import hk.freshnetwork.control.ExampleadminManager;
+import hk.freshnetwork.model.Beanadminfo;
+import hk.freshnetwork.model.Beanuser_table;
+import hk.freshnetwork.util.BaseException;
+
+public class FrmaddAdm extends JDialog implements ActionListener{
+	private JPanel toolBar = new JPanel();
+	private JPanel workPane = new JPanel();
+	
+	private JButton btnOk = new JButton("添加管理员");
+	private JButton btnCancel = new JButton("取消");	
+	private JLabel labelUser = new JLabel("管理员姓名:");
+	private JLabel labelPwd = new JLabel("密码：");
+	private JLabel labelPwd2 = new JLabel("确认密码：");
+	private JTextField edtUserId = new JTextField(20);
+	private JPasswordField edtPwd = new JPasswordField(20);
+	private JPasswordField edtPwd2 = new JPasswordField(20);	
+	
+	public FrmaddAdm(JFrame f, String s, boolean b) {
+		super(f, s, b);
+		toolBar.setLayout(new FlowLayout(FlowLayout.RIGHT));
+		toolBar.add(this.btnOk);
+		toolBar.add(btnCancel);
+		this.getContentPane().add(toolBar, BorderLayout.SOUTH);
+		workPane.add(labelUser);
+		workPane.add(edtUserId);
+		workPane.add(labelPwd);
+		workPane.add(edtPwd);
+		workPane.add(labelPwd2);
+		workPane.add(edtPwd2);
+		this.getContentPane().add(workPane, BorderLayout.CENTER);
+		this.setSize(260, 400);
+		this.btnCancel.addActionListener(this);
+		this.btnOk.addActionListener(this);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==this.btnCancel)
+			this.setVisible(false);
+		else if(e.getSource()==this.btnOk){
+			String adminname=this.edtUserId.getText();
+			String pwd1=new String(this.edtPwd.getPassword());
+			String pwd2=new String(this.edtPwd2.getPassword());
+			ExampleadminManager sum = new ExampleadminManager();
+			try {
+				Beanadminfo admin=sum.reg(adminname,pwd1,pwd2);
+				if(admin!=null) {
+					this.setVisible(false);
+					JOptionPane.showMessageDialog(null, "添加成功","成功",JOptionPane.INFORMATION_MESSAGE);
+				}
+				else {
+					JOptionPane.showMessageDialog(null, "添加失败","错误",JOptionPane.ERROR_MESSAGE);
+				}
+			} catch (BaseException e1) {
+				JOptionPane.showMessageDialog(null, e1.getMessage(),"错误",JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+		}
+	}
+}
