fe8f62ee24f4c5d3e1f7aa85eeda2e50aa66985d
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-05 16:30:47 +0800

    Update IadminManager.java

--- freshnetwork/src/hk/freshnetwork/itf/IadminManager.java
+++ freshnetwork/src/hk/freshnetwork/itf/IadminManager.java
@@ -6,5 +6,7 @@ import hk.freshnetwork.util.BaseException;
 public interface IadminManager {
 
 	Beanadminfo login(String Emp_name, String pwd) throws BaseException;
+	public Beanadminfo reg(String adminname, String pwd,String pwd2) throws BaseException;
+	void changePwd(Beanadminfo currentLoginadmin, String string, String string2, String string3) throws BaseException;
 
 }
