9154dec33a1f0713297f42e18c994dcb26f28498
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-05 16:30:49 +0800

    Update IUserManager.java

--- freshnetwork/src/hk/freshnetwork/itf/IUserManager.java
+++ freshnetwork/src/hk/freshnetwork/itf/IUserManager.java
@@ -6,6 +6,10 @@ import hk.freshnetwork.util.BaseException;
 public interface IUserManager {
 	public Beanuser_table reg(String username, String gender,String pwd,String pwd2,String phone,String mail,String city) throws BaseException;
 	public Beanuser_table login(String username, String pwd) throws BaseException;
-	public void changePwd(Beanuser_table user, String oldPwd, String newPwd,
+	public void changePwd(Beanuser_table currentLoginUser, String oldPwd, String newPwd,
 			String newPwd2) throws BaseException;
+    public void changephone(Beanuser_table user,String newphone) throws BaseException;
+    public void changemail(Beanuser_table user,String newmail) throws BaseException;
+    public void changecity(Beanuser_table user,String newcity) throws BaseException;
+    public void openvip(Beanuser_table user,String vip) throws BaseException;
 }
