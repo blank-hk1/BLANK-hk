512c3514ac83da917d7b8fa80c747dd9aaeab576
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-06 17:18:59 +0800

    Update FreshNetUtil.java

--- freshnetwork/src/hk/freshnetwork/action/FreshNetUtil.java
+++ freshnetwork/src/hk/freshnetwork/action/FreshNetUtil.java
@@ -2,10 +2,16 @@ package hk.freshnetwork.action;
 
 import hk.freshnetwork.control.ExampleUserManager;
 import hk.freshnetwork.control.ExampleadminManager;
+import hk.freshnetwork.control.ExamplefreshManager;
+import hk.freshnetwork.control.ExampleComMananger;
 import hk.freshnetwork.itf.IUserManager;
 import hk.freshnetwork.itf.IadminManager;
+import hk.freshnetwork.itf.IcomManager;
+import hk.freshnetwork.itf.IfreshManager;
 
 public class FreshNetUtil {
 	public static IUserManager userManager=  new ExampleUserManager();//��Ҫ����������Ƶ�ʵ����
 	public static IadminManager adminManager=  new ExampleadminManager();//��Ҫ����������Ƶ�ʵ����
+	public static IfreshManager freshManager=  new ExamplefreshManager();
+	public static IcomManager comManager=  new ExampleComMananger();
 }
