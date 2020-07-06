edb5437dcae895bad197565c7f12ef658d518bbb
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-06 17:19:01 +0800

    Update FreshStarter.java

--- freshnetwork/src/hk/freshnetwork/action/FreshStarter.java
+++ freshnetwork/src/hk/freshnetwork/action/FreshStarter.java
@@ -1,12 +1,14 @@
 package hk.freshnetwork.action;
 
 import hk.freshnetwork.ui.FrmMain;
+import hk.freshnetwork.util.BaseException;
 
 public class FreshStarter {
 	/**
 	 * @param args
+	 * @throws BaseException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws BaseException {
 		// TODO Auto-generated method stub
 		new FrmMain();
 	}
