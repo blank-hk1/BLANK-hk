8b927e5b2573b17979e68bf7dced3180cd230ba3
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-07 19:26:58 +0800

    Update IcomManager.java

--- freshnetwork/src/hk/freshnetwork/itf/IcomManager.java
+++ freshnetwork/src/hk/freshnetwork/itf/IcomManager.java
@@ -3,8 +3,14 @@ package hk.freshnetwork.itf;
 import java.util.List;
 
 import hk.freshnetwork.model.Beancommodity_information;
+import hk.freshnetwork.model.Beanfresh_information;
 import hk.freshnetwork.util.BaseException;
 
 public interface IcomManager {
 	public List<Beancommodity_information> loadcom()throws BaseException;
+	public List<Beancommodity_information> searchCom(String Trade_name)throws BaseException;
+	public Beancommodity_information regCom(String Trade_name,String category_number,String price,String member_price,String number,String Specifications,String details) throws BaseException;
+	public void modifyCom(String Trade_name,String category_number,String price,String member_price,String number,String Specifications,String details) throws BaseException;
+	public void deleteCom(String Trade_name);
+	public List<Beancommodity_information> loadcompur() throws BaseException;
 }
