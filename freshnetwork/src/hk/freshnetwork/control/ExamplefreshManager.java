b221b389f8d11f52701c9764f2511e1e41248407
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-07 19:26:53 +0800

    Update ExamplefreshManager.java

--- freshnetwork/src/hk/freshnetwork/control/ExamplefreshManager.java
+++ freshnetwork/src/hk/freshnetwork/control/ExamplefreshManager.java
@@ -8,6 +8,8 @@ import java.util.List;
 import hk.freshnetwork.itf.IUserManager;
 import hk.freshnetwork.itf.IfreshManager;
 import hk.freshnetwork.model.Beanfresh_information;
+import hk.freshnetwork.model.Beanmenu_info;
+import hk.freshnetwork.ui.FrmFreshMenu;
 import hk.freshnetwork.ui.FrmFreshcat;
 import hk.freshnetwork.util.BaseException;
 import hk.freshnetwork.util.BusinessException;
@@ -170,6 +172,194 @@ public class ExamplefreshManager implements IfreshManager{
 		}catch (SQLException e) {
 			e.printStackTrace();
 			throw new DbException(e);
+		}
+		finally{
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+	}
+	public void deleteFresh(String Category_name){
+		Connection conn=null;
+		try {
+			String str = null;
+			conn=DBUtil.getConnection();
+			String sql="delete from fresh_information where Category_name = ?";
+			java.sql.PreparedStatement pst=conn.prepareStatement(sql);
+			pst.setString(1, Category_name);
+			pst.execute();
+		    pst.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+		finally{
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+	}
+	public List<Beanmenu_info> loadmenu()throws BaseException{
+		List<Beanmenu_info> Menu = new ArrayList<Beanmenu_info>();
+		Connection conn=null;
+		try {
+			conn=DBUtil.getConnection();
+			String sql = "select * from menu_information";
+			
+			java.sql.PreparedStatement pst=conn.prepareStatement(sql);
+			java.sql.ResultSet rs=pst.executeQuery();					
+			while(rs.next()) {
+				Beanmenu_info me = new Beanmenu_info();
+				me.setMenu_number(rs.getInt(1));
+				me.setMenu_name(rs.getString(2));
+				me.setMenu_Materials(rs.getString(3));
+				me.setStep(rs.getString(4));
+				Menu.add(me);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		}
+		finally{
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		return Menu;
+	}
+	public List<Beanmenu_info> searchMenu(String menu_name)throws BaseException{
+		List<Beanmenu_info> Menu = new ArrayList<Beanmenu_info>();
+		Connection conn=null;
+		try {
+			conn=DBUtil.getConnection();
+			String sql = "select * from fresh_information where Category_name = ?";		
+			java.sql.PreparedStatement pst=conn.prepareStatement(sql);
+			pst.setString(1, menu_name);
+			java.sql.ResultSet rs=pst.executeQuery();			
+			while(rs.next()) {
+				Beanmenu_info me = new Beanmenu_info();
+				me.setMenu_number(rs.getInt(1));
+				me.setMenu_name(rs.getString(2));
+				me.setMenu_Materials(rs.getString(3));
+				me.setStep(rs.getString(4));
+				Menu.add(me);
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		}
+		finally{
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+		return Menu;
+	}
+	public void deleteMenu(String Menu_name){
+		Connection conn=null;
+		try {
+			String str = null;
+			conn=DBUtil.getConnection();
+			String sql="delete from menu_information where Menu_name = ?";
+			java.sql.PreparedStatement pst=conn.prepareStatement(sql);
+			pst.setString(1, Menu_name);
+			pst.execute();
+		    pst.close();
+		}catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+		finally{
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+	}
+	public Beanmenu_info regMenu(String menuname,String menuMaterials,String step) throws BaseException{
+		Connection conn=null;
+		try {
+			conn=DBUtil.getConnection();
+			String sql = "select * from menu_information where Menu_name = ?";
+			java.sql.PreparedStatement pst=conn.prepareStatement(sql);
+			pst.setString(1, menuname);
+			java.sql.ResultSet rs=pst.executeQuery();
+			if(rs.next()) throw new BusinessException("登陆账号已经存在");
+			rs.close();
+			pst.close();
+			
+			sql="insert into menu_information(Menu_name,Menu_Materials,step) values(?,?,?)";
+			pst=conn.prepareStatement(sql);
+			pst.setString(1, menuname);
+			pst.setString(2, menuMaterials);
+			pst.setString(3, step);
+			Beanmenu_info menu = new Beanmenu_info();
+			menu.setMenu_name(menuname);
+			menu.setMenu_Materials(menuMaterials);
+			menu.setStep(step);
+			pst.execute();
+			pst.close();
+			
+			return menu;
+		}catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		}
+		finally{
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
+	}
+	public void modifyMenu(String menuname, String state,String step){
+		// TODO Auto-generated method stub
+		Connection conn=null;
+		try {
+		    int t = 0;
+			conn=DBUtil.getConnection();
+			String sql = "select Menu_number from menu_information where Menu_name = ?";
+			java.sql.PreparedStatement pst=conn.prepareStatement(sql);
+			pst.setString(1,FrmFreshMenu.menus.getMenu_name());
+			java.sql.ResultSet rs=pst.executeQuery();
+			if(rs.next()) {
+				t=rs.getInt(1);
+			}
+			rs.close();
+			pst.close();
+			System.out.println("1"+state);
+			sql="update menu_information set  Menu_name= ?,Menu_Materials= ?,step=? where Menu_number=?";
+			pst=conn.prepareStatement(sql);
+				pst.setString(1, menuname);
+				pst.setString(2, state);
+				pst.setString(3, step);
+				pst.setInt(4, t);
+				pst.execute();
+				pst.close();	
+		}catch (SQLException e) {
+			e.printStackTrace();
 		}
 		finally{
 			if(conn!=null)
