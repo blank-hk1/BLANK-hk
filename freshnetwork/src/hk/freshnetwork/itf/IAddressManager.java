9c7010b72d0d27e0ac4cc118569124c001182a6d
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-09 15:42:14 +0800

    Create IAddressManager.java

---
+++ freshnetwork/src/hk/freshnetwork/itf/IAddressManager.java
@@ -0,0 +1,13 @@
+package hk.freshnetwork.itf;
+
+import java.util.List;
+
+import hk.freshnetwork.model.Beanaddlist;
+import hk.freshnetwork.util.BaseException;
+
+public interface IAddressManager {
+	public List<Beanaddlist> loadAddress()throws BaseException;
+	public Beanaddlist regAdd(String qu,String address,String contacts,String con_phone) throws BaseException;
+	public void modifyFresh(int add_number,String qu,String address,String contacts,String con_phone) throws BaseException;
+	public void deleteAdd(int add_number);
+}
