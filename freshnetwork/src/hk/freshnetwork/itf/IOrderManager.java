2e9db30cff755ba8f813f7f61c5e54376a06aec2
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-11 15:13:42 +0800

    Update IOrderManager.java

--- freshnetwork/src/hk/freshnetwork/itf/IOrderManager.java
+++ freshnetwork/src/hk/freshnetwork/itf/IOrderManager.java
@@ -3,15 +3,24 @@ package hk.freshnetwork.itf;
 import java.util.List;
 
 import hk.freshnetwork.model.BeanShopping;
-import hk.freshnetwork.model.Beanorder_details;
+import hk.freshnetwork.model.Beanaddlist;
+import hk.freshnetwork.model.Beancoupon;
 import hk.freshnetwork.model.Beanorder_form;
 import hk.freshnetwork.model.Beantime_pro;
 import hk.freshnetwork.util.BaseException;
+import hk.freshnetwork.util.BusinessException;
 
 public interface IOrderManager {
-	public List<BeanShopping> loadOrd()throws BaseException;
+	public List<BeanShopping> loadOrd(int usernum)throws BaseException;
 	public void RegOrdDetails(int Trade_number,int number,float money) throws BaseException;
 	public float sumPrice()throws BaseException;
 	public Beantime_pro searchPro(int Trade_number) throws BaseException;
 	public void deleteShop(int Trade_number);
+	public Beanorder_form CreatOrder(int User_num,int add_number,int Cou_number,float ori_money,float set_money);
+	public void deleteAllShop(int user_num);
+	public void updateCom(int username) throws BusinessException;
+	public List<Beanorder_form> loadRecords(int Usernum)throws BaseException;
+	public List<Beanorder_form> loadRecordStat(int Usernum,String state)throws BaseException;
+	public Beancoupon ReadCouName(int Cou_number) throws BaseException;
+	public Beanaddlist ReadAddName(int add_number) throws BaseException;
 }
