c1086b93fcf2f66e35dda02731409a1a8cfbb4d9
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-12 15:15:52 +0800

    Update IfreshManager.java

--- freshnetwork/src/hk/freshnetwork/itf/IfreshManager.java
+++ freshnetwork/src/hk/freshnetwork/itf/IfreshManager.java
@@ -5,15 +5,16 @@ import java.util.List;
 import hk.freshnetwork.model.Beanfresh_information;
 import hk.freshnetwork.model.Beanmenu_info;
 import hk.freshnetwork.util.BaseException;
+import hk.freshnetwork.util.BusinessException;
 
 public interface IfreshManager {
 	public List<Beanfresh_information> loadFresh()throws BaseException;
 	public void modifyFresh(String freshname, String state) throws BaseException;
 	public List<Beanfresh_information> searchFresh(String Category_name)throws BaseException;
 	public String getFreshname(int Category_number) throws BaseException;
-	public void deleteFresh(String category_name);
+	public void deleteFresh(String category_name) throws BusinessException;
 	public List<Beanmenu_info> loadmenu()throws BaseException;
 	public List<Beanmenu_info> searchMenu(String menu_name)throws BaseException;
-	public void deleteMenu(String menu_name);
+	public void deleteMenu(String menu_name) throws BusinessException;
 	public void modifyMenu(String menuname, String state, String step);
 }
