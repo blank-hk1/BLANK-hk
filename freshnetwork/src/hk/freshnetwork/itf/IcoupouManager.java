d3b80a3c846ed49d904cd5ba7682a171bba511d7
blank-hk1 <67767143+blank-hk1@users.noreply.github.com>
2020-07-13 21:26:24 +0800

    Update IcoupouManager.java

--- freshnetwork/src/hk/freshnetwork/itf/IcoupouManager.java
+++ freshnetwork/src/hk/freshnetwork/itf/IcoupouManager.java
@@ -7,14 +7,15 @@ import hk.freshnetwork.model.Beanfull_sheet;
 import hk.freshnetwork.model.Beanrelation;
 import hk.freshnetwork.model.Beantime_pro;
 import hk.freshnetwork.util.BaseException;
+import hk.freshnetwork.util.BusinessException;
 
 public interface IcoupouManager {
 	public List<Beancoupon> loadCou()throws BaseException;
 	public List<Beancoupon> SearchCou(float App_money)throws BaseException;
 	public Beancoupon RegCou(String content,float App_money,float Ded_money,String Start,int month) throws BaseException;
-	public void deleteCou(int Cou_number);
+	public void deleteCou(int Cou_number) throws BusinessException;
 	public void modifyCou(int Cou_number,String content,float App_money,float Ded_money,String Start,int month) throws BaseException;
-	public void deletePro(int Pro_number);
+	public void deletePro(int Pro_number) throws BusinessException;
 	public List<Beantime_pro> loadTimeSearch(int Pronumber)throws BaseException;
 	public List<Beantime_pro> loadTime()throws BaseException;
 	public Beantime_pro RegPro(int Trade_number,float Pro_price,int Prom_number,String ProStart_date,String ProEnd_date) throws BaseException;
@@ -23,8 +24,8 @@ public interface IcoupouManager {
 	public List<Beanfull_sheet> loadFullSearch(int Full_number)throws BaseException;
 	public Beanfull_sheet RegFul(String Full_content,int App_number,Float Discount,String FulStart_date,String FulEnd_date) throws BaseException;
 	public void modifyFul(int Full_number,String Full_content,int App_number,Float Discount,String FulStart_date,String FulEnd_date) throws BaseException;
-	public void deleteFul(int Full_number);
-	public void deleteFulCom(int Full_number);
+	public void deleteFul(int Full_number) throws BusinessException;
+	public void deleteFulCom(int Full_number,int Trade_number);
 	public void modifyFulCom(int Full_number,int Trade_number) throws BaseException;
 	public Beanrelation RegFulCom(int Ful_number,int Trade_number,String FulStart_date,String FulEnd_date) throws BaseException;
 	public List<Beanrelation> loadDisComSearch(int Full_number)throws BaseException;
